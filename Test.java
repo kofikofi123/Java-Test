4b14304ff645aa4390befc59674ffe5e45179d5f
kofikofi123 <usingnamespaceasd@gmail.com>
2016-11-21 23:46:01 -0500

    ops

--- Test.java
+++ Test.java
@@ -5,4 +5,11 @@ public class Test {
   public static int test_method(int test_argument){
       return test_argument*test_argument;
   }
+
+  public static void test_sample(void){
+  	int i;
+
+  	for (i = 0; i < 100; i++)
+  		;
+  }
 }
